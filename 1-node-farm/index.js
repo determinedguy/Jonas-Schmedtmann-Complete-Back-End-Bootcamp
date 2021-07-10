3c652df323779779b3a456f53108f0c8d7be3229
determinedguy <mhd.athallah@gmail.com>
2021-07-10 22:58:59 +0700

    Finished until 8th video

---
+++ 1-node-farm/index.js
@@ -0,0 +1,9 @@
+// Enabling file system access by requiring filesystem module
+const fs = require('fs');
+
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+console.log(textIn);
+
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log("File written!");
